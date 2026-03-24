c35e8664309898212cf7c94ff16a98e3e4501b11
paulinadrabik61017 <paulina.drabik123@onet.pl>
2026-03-24 09:51:08 +0100

    cos jump

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -4,11 +4,14 @@ using UnityEngine.InputSystem;
 public class PlayerMotor : MonoBehaviour
 {
     Vector2 direction;
+    private bool canJump = true;
+    private Rigidbody2D rigidbody2D;
     public float speed = 10;
+    public float jumpForce = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     private void Update()
@@ -19,5 +22,19 @@ public class PlayerMotor : MonoBehaviour
     private void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>();
+    }
+    
+    private void OnJump()
+    {
+        if ( canJump)
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
     }
 }
