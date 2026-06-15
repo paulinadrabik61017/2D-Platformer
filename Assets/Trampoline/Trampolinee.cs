ea069fb929cb5820c6b410acf0abffa83d1c3cc4
paulinadrabik61017 <paulina.drabik123@onet.pl>
2026-06-15 18:52:21 +0200

    kolejne

--- Assets/Trampoline/Trampolinee.cs
+++ Assets/Trampoline/Trampolinee.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
-
 public class Trampolinee : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Ustawienia Trampoliny")]
+    public float bounceForce = 15f; // Si³a wybicia w górê
+                                    // Funkcja uruchamia siê automatycznie, gdy coœ dotknie klocka
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
+        // Sprawdzamy, czy obiekt, który na nas skoczy³, ma Rigidbody2D (czyli czy to postaæ)
+        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            // Resetujemy prêdkoœæ pionow¹ postaci, ¿eby zawsze wybija³a siê tak samo mocno...
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            // ...i dodajemy potê¿ny impuls w górê!
+            rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+        }
     }
 }
